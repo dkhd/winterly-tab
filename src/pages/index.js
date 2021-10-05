5ed07a360c9e063c9268fe53156b84d955971d99
Diky Hadna <dikyhd@gmail.com>
2021-10-05 14:52:52 +0700

    feat: adding fullscreen support

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Blurhash } from "react-blurhash";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 import Clock from "../components/clock";
 
 function Home(props) {
   const [data, setData] = useState({});
+
+  const handleFullscreen = useFullScreenHandle();
+
   useEffect(() => {
     retrieveImage();
     const timer = setInterval(() => {
@@ -28,53 +32,63 @@ function Home(props) {
   return (
     <div>
       {data.hasOwnProperty("blur_hash") ? (
-        <div className="w-screen h-screen overflow-hidden">
-          <Blurhash hash={data.blur_hash} width="100%" height="100%" />
-          <div
-            className="absolute top-0 left-0 w-screen min-h-screen"
-            style={{
-              backgroundImage: "url(" + data.urls.full + ")",
-              backgroundSize: "cover",
-              backgroundPosition: "center center",
-              backgroundRepeat: "no-repeat",
-            }}
-          />
-          <div className="absolute bottom-5 left-5 flex flex-row w-100 text-sm text-white p-3 bg-opacity-10 bg-black">
-            <Clock></Clock>
-          </div>
-          <div className="absolute bottom-5 right-5 flex flex-row w-100 text-sm text-white p-3 bg-opacity-10 bg-black">
-            <span>
-              Photo by{" "}
-              <a
-                href={data.links.html}
-                target="_blank"
-                rel="noreferrer"
-                className="font-medium"
+        <FullScreen handle={handleFullscreen}>
+          <div className="w-screen h-screen overflow-hidden">
+            <Blurhash hash={data.blur_hash} width="100%" height="100%" />
+            <div
+              className="absolute top-0 left-0 w-screen min-h-screen"
+              style={{
+                backgroundImage: "url(" + data.urls.full + ")",
+                backgroundSize: "cover",
+                backgroundPosition: "center center",
+                backgroundRepeat: "no-repeat",
+              }}
+            />
+            Any fullscreen content here
+            <div className="absolute bottom-5 left-5 flex flex-row w-100 text-sm text-white p-3 bg-opacity-10 bg-black">
+              <Clock></Clock>
+            </div>
+            <div className="absolute bottom-5 right-5 flex flex-row w-100 text-sm text-white p-3 bg-opacity-10 bg-black">
+              <span>
+                Photo by{" "}
+                <a
+                  href={data.links.html}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="font-medium"
+                >
+                  {data.user.name}
+                </a>
+              </span>
+              &nbsp; &middot; &nbsp;
+              <button
+                className="flex flex-row items-center gap-1"
+                onClick={retrieveImage}
               >
-                {data.user.name}
-              </a>
-            </span>
-            &nbsp; &middot; &nbsp;
-            <button
-              className="flex flex-row items-center gap-1"
-              onClick={retrieveImage}
-            >
-              <span className="font-medium">Change Background</span>
-            </button>{" "}
-            &nbsp; &middot; &nbsp;
-            <span>
-              Winterly Tab by{" "}
-              <a
-                href="https://hadna.space"
-                target="_blank"
-                rel="noreferrer"
-                className="font-medium"
+                <span className="font-medium">Change Background</span>
+              </button>{" "}
+              &nbsp; &middot; &nbsp;
+              <button
+                className="flex flex-row items-center gap-1 font-medium"
+                onClick={handleFullscreen.enter}
               >
-                Diky Hadna
-              </a>
-            </span>
+                Fullscreen
+              </button>
+              &nbsp; &middot; &nbsp;
+              <span>
+                Winterly Tab by{" "}
+                <a
+                  href="https://hadna.space"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="font-medium"
+                >
+                  Diky Hadna
+                </a>
+              </span>
+            </div>
           </div>
-        </div>
+        </FullScreen>
       ) : null}
     </div>
   );
