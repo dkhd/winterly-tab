33f7896d3e1456a921d822b5e7343a079d2bddd3
Tanjiro <apoorveshukla2001@gmail.com>
2021-10-15 22:40:57 +0530

    Added multiple search engines

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ import Clock from "../components/clock";
 import About from "../components/about";
 
 function Home(props) {
+  let [baseURL, toggleBaseURL] = useState("https://www.google.com");
+  const [showSearch, toggleSearch] = useState(["visible", "invisible"]);
   const [data, setData] = useState({});
   const [openAbout, setOpenAbout] = useState(false);
   const [searchText, setSearchText] = useState("");
@@ -14,10 +16,16 @@ function Home(props) {
   const handleFullscreen = useFullScreenHandle();
 
   const handleSearch = (e) => {
-    if (e.key === "Enter" && searchText.length){
-      window.location = `https://www.google.com/search?q=${searchText}`
+    if (e.key === "Enter" && searchText.length) {
+      window.location = `${baseURL}/search?q=${searchText}`
     }
   }
+
+  useEffect(
+    () => {
+      toggleBaseURL(showSearch[0] == "visible" ? "https://www.google.com" : "https://www.bing.com");
+    }
+    , showSearch);
 
   useEffect(() => {
     const update = async () => {
@@ -58,7 +66,11 @@ function Home(props) {
                 backgroundRepeat: "no-repeat",
               }}
             />
-            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 p-3">
+            <div className="flex absolute top-0 left-1/2 transform -translate-x-1/2 p-3">
+              <div className="flex text-white mx-2 place-items-center items-center h-12 w-12 rounded-md bg-opacity-20 overflow-hidden">
+                <svg className={"h-8 w-8 text-white-500 " + showSearch[0]} width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M17.788 5.108A9 9 0 1021 12h-8" /></svg>
+                <svg className={showSearch[1]} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><path d="M6.1 0l6.392 2.25v22.5l9.004-5.198-4.414-2.07-2.785-6.932 14.186 4.984v7.246L12.497 32 6.1 28.442z" fill="white" /></svg>
+              </div>
               <div className="relative flex items-center w-third-screen h-12 rounded-md bg-opacity-20 bg-black overflow-hidden">
                 <div className="grid place-items-center h-full w-12 text-white">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -113,6 +125,20 @@ function Home(props) {
                 onClick={() => { handleAboutModal(true) }}
               >
                 About
+              </button>
+              &nbsp; &middot; &nbsp;
+              <button
+                className="flex flex-row items-center gap-1 font-medium"
+                onClick={() => {
+                  if (showSearch[0] == "visible") {
+                    toggleSearch(["invisible", "visible"]);
+                  } else {
+                    toggleSearch(["visible", "invisible"]);
+                  }
+
+                }}
+              >
+                Change search Engine
               </button>
             </div>
           </div>
