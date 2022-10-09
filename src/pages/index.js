4132ccf6c79cdcef4eb09ee6457223f805962cfb
Jeff Canale <canalejeff@gmail.com>
2022-10-09 17:40:23 +0800

    Enhanced search UI; Addd yahoo search

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,10 @@ import AutorenewIcon from '@mui/icons-material/Autorenew';
 import CameraIcon from '@mui/icons-material/Camera';
 import SearchIcon from '@mui/icons-material/Search';
 
+import { ReactComponent as BingLogo } from "../svg/bing.svg";
+import { ReactComponent as GoogleLogo } from "../svg/google.svg";
+import { ReactComponent as YahooLogo } from "../svg/yahoo.svg";
+
 import Clock from "../components/clock";
 import About from "../components/about";
 import Quote from "../components/quotes";
@@ -15,8 +19,8 @@ import Tooltip from "../components/Tooltip/tooltip";
 
 function Home(props) {
   let [baseURL, toggleBaseURL] = useState("https://www.google.com");
-  const [showSearch, toggleSearch] = useState(["visible", "invisible"]);
-  const [dim, setDim] = useState("")
+  const [showSearch, toggleSearch] = useState("google");
+  const [dim, setDim] = useState("");
   const [data, setData] = useState({});
   const [openAbout, setOpenAbout] = useState(false);
   const [searchText, setSearchText] = useState("");
@@ -29,11 +33,20 @@ function Home(props) {
     }
   }
 
-  useEffect(
-    () => {
-      toggleBaseURL(showSearch[0] === "visible" ? "https://www.google.com" : "https://www.bing.com");
+  useEffect(() => {
+    switch (showSearch) {
+      case "yahoo":
+        toggleBaseURL("https://search.yahoo.com")
+        break;
+      case "bing":
+        toggleBaseURL("https://www.bing.com")
+        break;
+    
+      default:
+        toggleBaseURL("https://www.google.com")
+        break;
     }
-    , [showSearch]);
+  }, [showSearch]);
 
   useEffect(() => {
     const update = async () => {
@@ -58,6 +71,36 @@ function Home(props) {
     setOpenAbout(value)
   }
 
+  const changeSearchEngine = () => {
+    const engines = ["google", "bing", "yahoo"];
+    let idx = 0;
+
+    const engineIdx = engines.findIndex(e => showSearch === e);
+
+    if (engineIdx > -1) idx = engineIdx+1;
+    
+    if(idx > 2) idx = 0;
+    
+    toggleSearch(engines[idx])
+  }
+
+  const renderSearchIcon = (engine) => {
+    let cmp;
+    switch (showSearch) {
+      case "bing":
+        cmp =  <BingLogo className="h-full w-full" />
+        break;
+      case "yahoo":
+        cmp =  <YahooLogo className="h-full w-full" />
+        break;
+    
+      default:
+        cmp =  <GoogleLogo className="h-full w-full" />
+        break;
+    }
+    return cmp
+  }
+
   return (
     <div>
       {data.hasOwnProperty("blur_hash") ? (
@@ -74,34 +117,30 @@ function Home(props) {
                 backgroundRepeat: "no-repeat",
               }}
             />
-            <div className="flex absolute top-0 left-1/2 transform -translate-x-1/2 p-3">
-              <div className="flex text-white mx-2 place-items-center items-center h-12 w-12 rounded-md bg-opacity-20 overflow-hidden">
-                <svg className={"h-8 w-8 text-white-500 " + showSearch[0]} width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M17.788 5.108A9 9 0 1021 12h-8" /></svg>
-                <svg className={showSearch[1]} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><path d="M6.1 0l6.392 2.25v22.5l9.004-5.198-4.414-2.07-2.785-6.932 14.186 4.984v7.246L12.497 32 6.1 28.442z" fill="white" /></svg>
-              </div>
-              <div className="relative flex items-center w-third-screen h-12 rounded-md bg-opacity-20 bg-black overflow-hidden">
-                <div className="grid place-items-center h-full w-12 text-white">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                  </svg>
-                </div>
 
-                <input
-                  onFocus={(e) => {
-                    setDim("filter brightness-50");
-                  }}
-                  onBlur={(e) => {
-                    setDim("");
-                  }}
-                  className="peer h-full w-full outline-none text-sm text-white pr-2 bg-transparent placeholder-gray-100"
-                  type="text"
-                  id="search"
-                  placeholder="Search something.."
-                  value={searchText}
-                  autoComplete="off"
-                  onChange={(e) => setSearchText(e.target.value)}
-                  onKeyDown={handleSearch}
-                />
+            <div className="flex absolute top-0 left-1/2 transform -translate-x-1/2 p-3 my-48">
+              <div className="grid grid-cols-12 grid-rows-1 rounded-md bg-opacity-40 bg-black overflow-hidden">
+                <div className="pl-4 col-span-2 h-14 w-full">
+                  { renderSearchIcon(showSearch) }
+                </div>
+                <div className="col-span-10 h-full w-full">
+                  <input
+                    onFocus={(e) => {
+                      setDim("filter brightness-50");
+                    }}
+                    onBlur={(e) => {
+                      setDim("");
+                    }}
+                    className="pl-4 h-full w-full outline-none text-sm text-white bg-transparent placeholder-gray-100"
+                    type="text"
+                    id="search"
+                    placeholder="Search something.."
+                    value={searchText}
+                    autoComplete="off"
+                    onChange={(e) => setSearchText(e.target.value)}
+                    onKeyDown={handleSearch}
+                  />
+                </div>
               </div>
             </div>
             <div className="absolute bottom-5 left-5 flex flex-row w-100 text-sm text-white p-3 bg-opacity-20 bg-black rounded-sm">
@@ -152,14 +191,7 @@ function Home(props) {
               <Tooltip tooltip="Change search engine" addon="break-word">
                 <button
                   className="flex flex-row items-center gap-1 font-medium hover:text-gray-200"
-                  onClick={() => {
-                    if (showSearch[0] === "visible") {
-                      toggleSearch(["invisible", "visible"]);
-                    } else {
-                      toggleSearch(["visible", "invisible"]);
-                    }
-
-                  }}
+                  onClick={changeSearchEngine}
                 >
                   <SearchIcon />
                 </button>
